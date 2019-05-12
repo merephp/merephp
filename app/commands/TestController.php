0d557d248b023dd622aa698a18ddbe3760250eb3
Nick Tsai <myintaer@gmail.com>
2019-05-12 20:55:08 +0800

    test

--- app/commands/TestController.php
+++ app/commands/TestController.php
@@ -4,5 +4,13 @@ namespace app\commands;
 
 class TestController
 {
-    
+    public function index($a=null, $b='')
+    {
+        echo "Hit\n";
+
+        echo "A: {$a}\n";
+        echo "B: {$b}\n";
+
+        return 'aaa';
+    }
 }
